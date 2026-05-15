7a57be98a222f58a7584203bd94559a00d10b48c
Joshua <josh.hdz956@yahoo.com>
2026-05-14 19:04:48 -0500

    minimal event display

--- app/src/main/java/com/example/joshuaEventApp/EventDisplayActivity.java
+++ app/src/main/java/com/example/joshuaEventApp/EventDisplayActivity.java
@@ -1,6 +1,24 @@
 package com.example.joshuaEventApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EventDisplayActivity extends AppCompatActivity {
+    private RecyclerView recycleViewEvents;
+    private FloatingActionButton addEvent;
+    private DatabaseHelper databaseHelper;
+    private SessionManager sessionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        databaseHelper = new DatabaseHelper(this);
+        sessionManager = new SessionManager(this);
+    }
 }
