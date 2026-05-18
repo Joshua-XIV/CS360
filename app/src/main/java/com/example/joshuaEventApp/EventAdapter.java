bb5256bdcb0c6e268c84424f0b2a219332479b4c
Joshua <josh.hdz956@yahoo.com>
2026-05-18 08:19:05 -0500

    clean up unused import

--- app/src/main/java/com/example/joshuaEventApp/EventAdapter.java
+++ app/src/main/java/com/example/joshuaEventApp/EventAdapter.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
